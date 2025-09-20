3a8aeb84aeca46efc565c4cee7cdbe63f5e3cc5a
Daniel <d@stuhrs.dk>
2025-09-20 20:18:07 +0200

    Remove logging

--- OpenPassVault.Web/Services/SubtleCryptoWrapper.cs
+++ OpenPassVault.Web/Services/SubtleCryptoWrapper.cs
@@ -4,29 +4,19 @@ using Microsoft.JSInterop;
 
 namespace OpenPassVault.Web.Services;
 
-public class SubtleCryptoWrapper(
-    ISymmetricKeyGenerator keyGenerator,
-    IJSRuntime jsRuntime,
-    ICryptoService cryptoService,
-    ILogger<SubtleCryptoWrapper> logger) : IEncryptionService
+public class SubtleCryptoWrapper(ISymmetricKeyGenerator keyGenerator, IJSRuntime jsRuntime) : IEncryptionService
 {
     private const int KeySize = 32;
     
     public async Task<string> Encrypt(string plainText, string password)
     {
-       
         var keyResponse = await keyGenerator.GenerateKey(password, KeySize);
-        logger.LogInformation($"Key: {keyResponse.KeyBase64}");
-        logger.LogInformation($"Salt: {keyResponse.SaltBase64}");
-
-        var cryptoService = new CryptoService(jsRuntime, new CryptoOptions()
+        var cryptoService = new CryptoService(jsRuntime, new CryptoOptions
         {
             Key = keyResponse.KeyBase64
         });
         
         var result = await cryptoService.EncryptAsync(plainText);
-        logger.LogInformation($"Key from output: {result.Secret.Key}, IV from output: {result.Secret.IV}");
-
         var fullCipherText = Convert.FromBase64String(result.Value)
             .Concat(keyResponse.Salt)
             .ToArray();
@@ -37,34 +27,16 @@ public class SubtleCryptoWrapper(
     public async Task<string> Decrypt(string cipherText, string password)
     {
         var fullCipherTextBytes = Convert.FromBase64String(cipherText);
-        logger.LogInformation($"Full cipher text length: {fullCipherTextBytes.Length}");
-        
         var cipherTextLength = fullCipherTextBytes.Length - keyGenerator.SaltLength;
-        logger.LogInformation($"Cipher text length: {cipherTextLength}");
         var cipherTextBytes = fullCipherTextBytes.Take(cipherTextLength).ToArray();
-        logger.LogInformation($"Cipher text bytes length: {cipherTextBytes.Length}");
-        
         
         var salt = fullCipherTextBytes.Skip(cipherTextLength).ToArray();
-        logger.LogInformation($"Salt length: {salt.Length}");
-        
         var keyResponse = await keyGenerator.GenerateKey(password, salt, KeySize);
-        logger.LogInformation($"Key: {keyResponse.KeyBase64}");
-        logger.LogInformation($"Salt: {keyResponse.SaltBase64}");
-        
-        var cryptoInput = new CryptoInput
-        {
-            Value = Convert.ToBase64String(cipherTextBytes),
-            Key = keyResponse.KeyBase64
-        };
-        
-        var service = new CryptoService(jsRuntime, new CryptoOptions()
+        var service = new CryptoService(jsRuntime, new CryptoOptions
         {
             Key = keyResponse.KeyBase64
         });
 
-        var decrypted = await service.DecryptAsync(cryptoInput.Value);
-        logger.LogInformation($"decrypted length: {decrypted.Length}, value: {decrypted}");
-        return decrypted;
+        return await service.DecryptAsync(Convert.ToBase64String(cipherTextBytes));
     }
 }
