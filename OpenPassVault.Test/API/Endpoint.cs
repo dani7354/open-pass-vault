534b71349b177a655c52e8f53b25f5340743ac24
Daniel <d@stuhrs.dk>
2025-11-15 12:58:56 +0100

    Add auth test for secret endpoints

--- OpenPassVault.Test/API/Endpoint.cs
+++ OpenPassVault.Test/API/Endpoint.cs
@@ -5,4 +5,7 @@ public static class Endpoint
     public const string AuthBaseEndpoint = "/api/auth";
     public const string AuthUserInfoEndpoint = $"{AuthBaseEndpoint}/user-info";
     public const string AuthDeleteEndpoint = $"{AuthBaseEndpoint}/delete";
+    
+    public const string SecretBaseEndpoint = "/api/secrets";
+    public const string SecretBatchUpdateEndpoint = $"{SecretBaseEndpoint}/batch";
 }
---
+++ OpenPassVault.Test/API/IntegrationTest/SecretControllerTest.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using OpenPassVault.Test.API.Setup;
+
+namespace OpenPassVault.Test.API.IntegrationTest;
+
+public class SecretControllerTest : ControllerTestBase
+{
+    public static IEnumerable<object[]> EndpointsWithAuthentication =>
+        new List<object[]>
+        {
+            new object[] { Endpoint.SecretBaseEndpoint, "GET" },
+            new object[] { Endpoint.SecretBaseEndpoint, "POST" },
+            new object[] { $"{Endpoint.SecretBaseEndpoint}/{Guid.NewGuid().ToString()}", "GET" },
+            new object[] { $"{Endpoint.SecretBaseEndpoint}/{Guid.NewGuid().ToString()}", "DELETE" },
+            new object[] { $"{Endpoint.SecretBaseEndpoint}/{Guid.NewGuid().ToString()}", "PUT" },
+            new object[] { Endpoint.SecretBatchUpdateEndpoint, "PUT" },
+        };
+    
+    [Theory]
+    [MemberData(nameof(EndpointsWithAuthentication))]
+    public async Task EndpointsRequireAuthentication_NotAuthenticated_ReturnsUnauthorized(
+        string endpoint, 
+        string httpMethod)
+    {
+        var client = Factory.CreateClient();
+        HttpResponseMessage? response = httpMethod switch
+        {
+            "GET" => await client.GetAsync(endpoint),
+            "POST" => await client.PostAsync(endpoint, null!),
+            "PUT" => await client.PutAsync(endpoint, null!),
+            "DELETE" => await client.DeleteAsync(endpoint),
+            _ => throw new NotSupportedException($"HTTP method {httpMethod} is not supported in this test.")
+        };
+
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+}
