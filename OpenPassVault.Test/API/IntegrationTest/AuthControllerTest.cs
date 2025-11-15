4680170bca9b73458f286223acf59f12af366f7d
Daniel <d@stuhrs.dk>
2025-11-15 18:47:55 +0100

    Refactor and add test in SecretControllerTest.cs

--- OpenPassVault.Test/API/IntegrationTest/AuthControllerTest.cs
+++ OpenPassVault.Test/API/IntegrationTest/AuthControllerTest.cs
@@ -1,26 +1,19 @@
 using System.Net;
-using System.Net.Http.Json;
-using OpenPassVault.Shared.DTO;
 using OpenPassVault.Test.API.Setup;
-using System.Text;
-using OpenPassVault.API.Helpers;
-using OpenPassVault.Shared.Crypto;
 
 namespace OpenPassVault.Test.API.IntegrationTest;
 
 public class AuthControllerTest : ControllerTestBase
 {
-    private const string TestUserEmail = "a-mail@gmail.com";
-    private const string TestUserPassword = "StrongPassword123!";
-    private const string TestUserMasterPasswordHash = "MasterPasswordHashExample";
-    
+
+
     public static IEnumerable<object[]> EndpointsWithAuthentication =>
         new List<object[]>
         {
             new object[] { Endpoint.AuthUserInfoEndpoint, "GET" },
             new object[] { Endpoint.AuthDeleteEndpoint, "DELETE" },
         };
-    
+
     public static IEnumerable<object[]> InvalidEmailAddresses =>
         new List<object[]>
         {
@@ -30,13 +23,13 @@ public class AuthControllerTest : ControllerTestBase
             new object[] { "username\n@com" },
             new object[] { "username@" },
         };
-    
+
     #region Tests
-    
+
     [Theory]
     [MemberData(nameof(EndpointsWithAuthentication))]
     public async Task EndpointsRequireAuthentication_NotAuthenticated_ReturnsUnauthorized(
-        string endpoint, 
+        string endpoint,
         string httpMethod)
     {
         var client = Factory.CreateClient();
@@ -49,45 +42,45 @@ public class AuthControllerTest : ControllerTestBase
 
         Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
     }
-    
+
     [Fact]
     public async Task Register_ValidInput_ReturnsSuccess()
     {
         var client = Factory.CreateClient();
-        await RegisterValidTestUser(client);
+        await AuthRequestHelper.RegisterValidTestUser(client);
     }
-    
+
     [Fact]
     public async Task Register_PasswordMismatch_ReturnsBadRequest()
     {
         var client = Factory.CreateClient();
-        var registerRequestPayload = await GetRegisterRequestPayload(
+        var registerRequestPayload = await AuthRequestHelper.GetRegisterRequestPayload(
             password: "Password1!",
             confirmPassword: "Password2!");
-        
+
         var response = await client.PostAsync(Endpoint.AuthRegisterEndpoint, registerRequestPayload);
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
-    
+
     [Theory]
     [MemberData(nameof(InvalidEmailAddresses))]
     public async Task Register_InvalidEmail_ReturnsBadRequest(string email)
     {
         var client = Factory.CreateClient();
-        var registerRequestPayload = await GetRegisterRequestPayload(
+        var registerRequestPayload = await AuthRequestHelper.GetRegisterRequestPayload(
             email: email);
-        
+
         var response = await client.PostAsync(Endpoint.AuthRegisterEndpoint, registerRequestPayload);
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
-    
+
     [Fact]
     public async Task Register_DuplicateEmail_ReturnsBadRequest()
     {
         var client = Factory.CreateClient();
-        await RegisterValidTestUser(client);
-        
-        var registerRequestPayload = await GetRegisterRequestPayload();
+        await AuthRequestHelper.RegisterValidTestUser(client);
+
+        var registerRequestPayload = await AuthRequestHelper.GetRegisterRequestPayload();
         var response = await client.PostAsync(Endpoint.AuthRegisterEndpoint, registerRequestPayload);
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
@@ -96,9 +89,9 @@ public class AuthControllerTest : ControllerTestBase
     public async Task Login_ValidInput_ReturnsSuccess()
     {
         var client = Factory.CreateClient();
-        await RegisterValidTestUser(client);
-        var tokenResponse = await LoginValidTestUser(client);
-        
+        await AuthRequestHelper.RegisterValidTestUser(client);
+        var (_, tokenResponse) = await AuthRequestHelper.LoginValidTestUser(client);
+
         Assert.True(!string.IsNullOrEmpty(tokenResponse.Token));
     }
 
@@ -106,74 +99,14 @@ public class AuthControllerTest : ControllerTestBase
     public async Task UserInfo_AfterLogin_ReturnsUserInfo()
     {
         var client = Factory.CreateClient();
-        
-        await RegisterValidTestUser(client);
-        var tokenResponse = await LoginValidTestUser(client);
-               
+
+        await AuthRequestHelper.RegisterValidTestUser(client);
+        var (_, tokenResponse) = await AuthRequestHelper.LoginValidTestUser(client);
+
         client.DefaultRequestHeaders.Add("Authorization", "Bearer " + tokenResponse.Token);
         var userInfoResponse = await client.GetAsync(Endpoint.AuthUserInfoEndpoint);
         Assert.Equal(HttpStatusCode.OK, userInfoResponse.StatusCode);
     }
-    #endregion
-    
-    #region Helpers
-    
-    private async Task RegisterValidTestUser(
-        HttpClient client,
-        string email = TestUserEmail,
-        string password = TestUserPassword)
-    {
-        var registerRequestPayload = await GetRegisterRequestPayload(
-            email: email, 
-            password: password, 
-            confirmPassword: password);
 
-        var response = await client.PostAsync(Endpoint.AuthRegisterEndpoint, registerRequestPayload);
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    private async Task<TokenResponse> LoginValidTestUser(
-        HttpClient client,
-        string email = TestUserEmail,
-        string password = TestUserPassword)
-    {
-        var loginRequest = new LoginRequest
-        {
-            Email = email,
-            Password = password
-        };
-        
-        var loginRequestPayload = HttpContentHelper.CreateStringContent(loginRequest);
-        var loginResponse = await client.PostAsync(Endpoint.AuthLoginEndpoint, loginRequestPayload);
-        var tokenReponse = await loginResponse.Content.ReadFromJsonAsync<TokenResponse>();
-        Assert.Equal(HttpStatusCode.OK, loginResponse.StatusCode);
-        Assert.True(!string.IsNullOrEmpty(tokenReponse?.Token));
-
-        return tokenReponse;
-    }
-
-    private async Task<StringContent> GetRegisterRequestPayload(
-        string email = TestUserEmail,
-        string password = TestUserPassword,
-        string confirmPassword = TestUserPassword,
-        string masterPasswordHash = TestUserMasterPasswordHash)
-    {
-        var captchaCode = "xy45xxswr";
-        var key = EnvironmentHelper.GetCsrfTokenKey();
-        var hmacService = new HmacService(key);
-        var captchaHmac = await hmacService.CreateHmac(Encoding.UTF8.GetBytes(captchaCode));
-        
-        var userRegisterRequest = new RegisterRequest
-        {
-            Email = email,
-            Password = password,
-            ConfirmPassword = confirmPassword,
-            MasterPasswordHash = masterPasswordHash,
-            CaptchaCode = captchaCode,
-            CaptchaHmac = captchaHmac
-        };
-        
-        return HttpContentHelper.CreateStringContent(userRegisterRequest);
-    }
     #endregion
 }
--- OpenPassVault.Test/API/IntegrationTest/SecretControllerTest.cs
+++ OpenPassVault.Test/API/IntegrationTest/SecretControllerTest.cs
@@ -1,10 +1,16 @@
 using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Net.Http.Headers;
+using OpenPassVault.Shared.DTO;
 using OpenPassVault.Test.API.Setup;
 
 namespace OpenPassVault.Test.API.IntegrationTest;
 
 public class SecretControllerTest : ControllerTestBase
 {
+    private HttpClient _authenticatedClient = null!;
+    
     public static IEnumerable<object[]> EndpointsWithAuthentication =>
         new List<object[]>
         {
@@ -15,6 +21,11 @@ public class SecretControllerTest : ControllerTestBase
             new object[] { $"{Endpoint.SecretBaseEndpoint}/{Guid.NewGuid().ToString()}", "PUT" },
             new object[] { Endpoint.SecretBatchUpdateEndpoint, "PUT" },
         };
+
+    public SecretControllerTest()
+    {
+        SetupAuthenticatedClientAsync().GetAwaiter().GetResult();
+    }
     
     [Theory]
     [MemberData(nameof(EndpointsWithAuthentication))]
@@ -33,5 +44,49 @@ public class SecretControllerTest : ControllerTestBase
         };
 
         Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task Secrets_NoSecrets_ReturnsEmptyArray()
+    {
+        var response = await _authenticatedClient.GetAsync(Endpoint.SecretBaseEndpoint);
+
+        response.EnsureSuccessStatusCode();
+
+        var secrets = await response.Content.ReadFromJsonAsync<SecretListItemResponse[]>();
+        Assert.NotNull(secrets);
+        Assert.Empty(secrets);
+    }
+
+    [Fact]
+    public async Task CreateSecret_ValidInput_ReturnsCreatedSecret()
+    {
+        var newSecretRequest = new SecretCreateRequest
+        {
+            Name = "Facebook Account",
+            Type = "Account",
+            Username = "mail@gmail.com",
+            Content = new string('*', 100),
+            Description = "Min gamle Facebook-konto"
+        };
+
+        var requestPayload = HttpContentHelper.CreateStringContent(newSecretRequest);
+        var response = await _authenticatedClient.PostAsync(Endpoint.SecretBaseEndpoint, requestPayload);
+
+        response.EnsureSuccessStatusCode();
+
+        var createdSecret = await response.Content.ReadFromJsonAsync<SecretDetailsResponse>();
+        Assert.NotNull(createdSecret);
+    }
+    
+
+    private async Task SetupAuthenticatedClientAsync()
+    {
+        _authenticatedClient = Factory.CreateClient();
+        await AuthRequestHelper.RegisterValidTestUser(_authenticatedClient);
+        var (csrfTokenCookie, tokenResponse) = await AuthRequestHelper.LoginValidTestUser(_authenticatedClient);
+        _authenticatedClient.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, tokenResponse.Token);
+        _authenticatedClient.DefaultRequestHeaders.Add(HeaderNames.Cookie, csrfTokenCookie);
     }
 }
